c4024a8849f41e4ef2ddc2bc19229f78256faa33
Tiff Chan <tiffchan@Tiffanys-MacBook-Air.local>
2023-12-17 17:53:34 -0500

    Added GET request to notes.html

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require("express");
+const fs = require("fs");
+const path = require("path");
 
 const PORT = 3001; 
 
 const app = express();
 
+const notesData = require('./db/db.json')
+
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
+
+app.get(`/notes`, (req, res) =>
+    res.sendFile(path.join(__dirname, 'public/notes.html'))
+);
 
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
