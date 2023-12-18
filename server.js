c319a7acfbf7cb229125b62c302b484d6c9d9a07
Tiff Chan <tiffchan@Tiffanys-MacBook-Air.local>
2023-12-17 22:15:18 -0500

    Added post request

--- server.js
+++ server.js
@@ -1,18 +1,31 @@
 const express = require("express");
-const fs = require("fs");
+const app = express();
 const path = require("path");
+
+const fs = require("fs");
 
 const PORT = 3001; 
 
-const app = express();
-
-const notesData = require('./db/db.json')
+const notesData = require('./db/db.json');
 
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
+app.use(express.json())
+
+app.get("/", (req, res) => 
+    res.sendFile(path.join(__dirname), 'public/index.html')
+);
 
 app.get(`/notes`, (req, res) =>
     res.sendFile(path.join(__dirname, 'public/notes.html'))
+);
+
+app.get(`/api/notes`, (req, res) =>
+    res.json(notesData)
+);
+
+app.post(`/api/notes`, (req, res) => 
+    res.json({message: `New note is created: ${req.body.title}`})
 );
 
 app.listen(PORT, () =>
